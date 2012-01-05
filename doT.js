2f160e045a67294f60276980ab184e461f1961f2
Danny <danny200bc@aol.com>
2012-01-05 12:21:47 +0200

    First shot: 1) Remove need for "it" as prefix for data 2) add auto loop control via {{* and exposing "li" as current object. 3) allow manual deeper context via {{~ Does not work for nested loops, needs more testing, perhaps some unnecessary regexes

--- doT.js
+++ doT.js
@@ -26,9 +26,13 @@
 		encode:      /\{\{!([\s\S]+?)\}\}/g,
 		use:         /\{\{#([\s\S]+?)\}\}/g, //compile time evaluation
 		define:      /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g, //compile time defs
-		conditionalStart: /\{\{\?([\s\S]+?)\}\}/g,
+		conditionalStart: /\{\{\?([\s\S]+?)\}\}/g,  //runtime conditional
 		conditionalEnd: /\{\{\?\}\}/g,
-		varname: 'it',
+		loopStart: /\{\{\*([\s\S]+?)\}\}/g,  //loop, shortcut for writing for, etc. and gives variable "li" as the looped object
+		loopEnd: /\{\{\*\}\}/g,
+		contextStart: /\{\{\~([\s\S]+?)\}\}/g, //change context for partials or whatever
+		contextEnd: /\{\{\~\}\}/g,
+		//varname: 'it', //should not be configurable
 		strip : true,
 		append: true
 	};
@@ -56,25 +60,40 @@
 
 	doT.template = function(tmpl, c, def) {
 		c = c || doT.templateSettings;
-		var cstart = c.append ? "'+(" : "';out+=(", // optimal choice depends on platform/size of templates
-		    cend   = c.append ? ")+'" : ");out+='";
+		var context = "ctx.",
+		 cstart = c.append ? "'+(" : "';out+=(", // optimal choice depends on platform/size of templates
+		 cend   = c.append ? ")+'" : ");out+='";
 		var str = (c.use || c.define) ? resolveDefs(c, tmpl, def || {}) : tmpl;
 
-		str = ("var out='" +
+		str = ("var ctx = it; var out='" +
 			((c.strip) ? str.replace(/\s*<!\[CDATA\[\s*|\s*\]\]>\s*|[\r\n\t]|(\/\*[\s\S]*?\*\/)/g, ''): str)
 			.replace(/\\/g, '\\\\')
 			.replace(/'/g, "\\'")
 			.replace(c.interpolate, function(match, code) {
-				return cstart + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ') + cend;
+				return cstart + context + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ') + cend;
 			})
 			.replace(c.encode, function(match, code) {
-				return cstart + code.replace(/\\'/g, "'").replace(/\\\\/g, "\\").replace(/[\r\t\n]/g, ' ') + ").toString().replace(/&(?!\\w+;)/g, '&#38;').split('<').join('&#60;').split('>').join('&#62;').split('" + '"' + "').join('&#34;').split(" + '"' + "'" + '"' + ").join('&#39;').split('/').join('&#47;'" + cend;
+				return cstart + context + code.replace(/\\'/g, "'").replace(/\\\\/g, "\\").replace(/[\r\t\n]/g, ' ') + ").toString().replace(/&(?!\\w+;)/g, '&#38;').split('<').join('&#60;').split('>').join('&#62;').split('" + '"' + "').join('&#34;').split(" + '"' + "'" + '"' + ").join('&#39;').split('/').join('&#47;'" + cend;
 			})
 			.replace(c.conditionalEnd, function(match, expression) {
 				return "';}out+='";
 			})
 			.replace(c.conditionalStart, function(match, expression) {
-				var code = "if(" + expression + "){";
+				var code = "if(" + context + expression + "){";
+				return "';" + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ')  + "out+='";
+			})
+			.replace(c.loopEnd, function(match, array) {
+				return "';} ctx = it; out+='";
+			})
+			.replace(c.loopStart, function(match, array) {
+				var code = "for (var i = 0, li, larray = "+ context + array + "; li = larray[i++];){ ctx = {li:li}; ";
+				return "';" + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ')  + "out+='";
+			})
+			.replace(c.contextEnd, function(match, context) {
+				return "'; ctx = it; out+='";
+			})
+			.replace(c.contextStart, function(match, context) {
+				var code = "ctx = it." + context + "; ";
 				return "';" + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ')  + "out+='";
 			})
 			.replace(c.evaluate, function(match, code) {
@@ -88,7 +107,7 @@
 			.split("var out='';out+=").join('var out=');
 
 		try {
-			return new Function(c.varname, str);
+			return new Function("it", str);
 		} catch (e) {
 			if (typeof console !== 'undefined') console.log("Could not create a template function: " + str);
 			throw e;
